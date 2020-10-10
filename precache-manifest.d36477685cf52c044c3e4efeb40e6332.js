bd707996765e930c4255ae47ae8db6388df7aed8
jokotoye18 <jokotoyeademola95@gmail.com>
2020-10-10 21:07:00 +0100

    Updates

---
+++ precache-manifest.d36477685cf52c044c3e4efeb40e6332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeda7058ab76b85c74b81c97f7f4c7f4",
+    "url": "/react-meme-generator/index.html"
+  },
+  {
+    "revision": "a42619d863dc078c50be",
+    "url": "/react-meme-generator/static/css/main.f6932405.chunk.css"
+  },
+  {
+    "revision": "d39c5c612afff5164787",
+    "url": "/react-meme-generator/static/js/2.ccd7139d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-meme-generator/static/js/2.ccd7139d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42619d863dc078c50be",
+    "url": "/react-meme-generator/static/js/main.e7006901.chunk.js"
+  },
+  {
+    "revision": "4f9d1ce1d7ffabd8a181",
+    "url": "/react-meme-generator/static/js/runtime-main.f8a4d7a3.js"
+  },
+  {
+    "revision": "868420eb188d33c1ac602114326e4a40",
+    "url": "/react-meme-generator/static/media/13.868420eb.jpg"
+  }
+]);
